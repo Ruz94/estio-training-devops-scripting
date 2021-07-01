172d3316e23d20a064eb8dc5a7c2b59de8918d77
Ruzina Begum <ruzina75@gmail.com>
2021-07-01 14:03:07 +0100

    minor change for user input

--- project/drinks.py
+++ project/drinks.py
@@ -30,14 +30,16 @@ counter = 0
 for drink in drinkChoices:
     counter += 1
 print(counter, " ", drinkChoices)
-drinkChoice = int(input("What drink would you like: "))
+drinkChoice = int(input(
+    "Please select a coffee. Americano is 1, Latte is 2, Cappucino is 3, Mocha is 4, Flat White is 5:"))
 
 counter = 0
 
 for size in drinkSizes:
     counter += 1
 print(counter, " ", drinkSizes)
-drinkSize = int(input("What size would you like: "))
+drinkSize = int(
+    input("Please select a size, Small is 1, Medium is 2, Large is 3:"))
 
 setChoice(drinkChoices[drinkChoice-1])
 setSize(drinkSizes[drinkSize-1])
